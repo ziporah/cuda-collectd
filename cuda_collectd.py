ed24b1f178ab32cd8bf6278e9b434491aaaa59a8
Fabien Wernli <cpan@faxm0dem.org>
2013-08-19 14:15:47 +0200

    should work using py2.6

--- cuda_collectd.py
+++ cuda_collectd.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 import collectd
 import subprocess
 import xml.etree.ElementTree as ET
@@ -8,20 +6,29 @@ def read(data=None):
         vl = collectd.Values(type='gauge')
         vl.plugin = 'cuda'
 
-        out = subprocess.check_output(['nvidia-smi', '-q', '-x'])
+	signal.signal(SIGCHLD, signal.SIG_DFL)
+        out = subprocess.Popen(['nvidia-smi', '-q', '-x', '-d', 'MEMORY,UTILIZATION,POWER' ], stdout=subprocess.PIPE).communicate()[0]
         root = ET.fromstring(out)
 
-        for gpu in root.iter('gpu'):
+        for gpu in root.getiterator('gpu'):
                 vl.plugin_instance = 'cuda-%s' % (gpu.attrib['id'])
 
-                vl.dispatch(type='fanspeed',
-                            values=[float(gpu.find('fan_speed').text.split()[0])])
+#                vl.dispatch(type='fanspeed',
+#                            values=[float(gpu.find('fan_speed').text.split()[0])])
 
-                vl.dispatch(type='temperature',
-                            values=[float(gpu.find('temperature/gpu_temp').text.split()[0])])
+#                vl.dispatch(type='temperature',
+#                            values=[float(gpu.find('temperature/gpu_temp').text.split()[0])])
 
                 vl.dispatch(type='memory', type_instance='used',
                             values=[1e6 * float(gpu.find('memory_usage/used').text.split()[0])])
+
+                vl.dispatch(type='memory', type_instance='total',
+                            values=[1e6 * float(gpu.find('memory_usage/total').text.split()[0])])
+
+                vl.dispatch(type='percent', type_instance='gpu_util',
+                            values=[float(gpu.find('utilization/gpu_util').text.split()[0])])
+                vl.dispatch(type='power', type_instance='draw',
+                            values=[float(gpu.find('power_readings/power_draw').text.split()[0])])
 
 collectd.register_read(read)
 
