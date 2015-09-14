71431f6f253f15eebc1163270804154510a3a80a
root <root@r10n9.thinking.leuven.vsc>
2015-09-14 10:04:32 +0200

    Altered code to import signal Added memory_util and changed memory to fb_memory nvidia-smi doesn't support using -x and -d option simultaniously anymore

--- cuda_collectd.py
+++ cuda_collectd.py
@@ -1,13 +1,18 @@
 import collectd
+import signal
 import subprocess
 import xml.etree.ElementTree as ET
+
+
+def restore_sigchld():
+    signal.signal(signal.SIGCHLD, signal.SIG_DFL)
 
 def read(data=None):
         vl = collectd.Values(type='gauge')
         vl.plugin = 'cuda'
 
-	signal.signal(SIGCHLD, signal.SIG_DFL)
-        out = subprocess.Popen(['nvidia-smi', '-q', '-x', '-d', 'MEMORY,UTILIZATION,POWER' ], stdout=subprocess.PIPE).communicate()[0]
+#	signal.signal(SIGCHLD, signal.SIG_DFL)
+        out = subprocess.Popen(['nvidia-smi', '-q', '-x' ], stdout=subprocess.PIPE).communicate()[0]
         root = ET.fromstring(out)
 
         for gpu in root.getiterator('gpu'):
@@ -20,15 +25,18 @@ def read(data=None):
 #                            values=[float(gpu.find('temperature/gpu_temp').text.split()[0])])
 
                 vl.dispatch(type='memory', type_instance='used',
-                            values=[1e6 * float(gpu.find('memory_usage/used').text.split()[0])])
+                            values=[1e6 * float(gpu.find('fb_memory_usage/used').text.split()[0])])
 
                 vl.dispatch(type='memory', type_instance='total',
-                            values=[1e6 * float(gpu.find('memory_usage/total').text.split()[0])])
+                            values=[1e6 * float(gpu.find('fb_memory_usage/total').text.split()[0])])
 
                 vl.dispatch(type='percent', type_instance='gpu_util',
                             values=[float(gpu.find('utilization/gpu_util').text.split()[0])])
+                vl.dispatch(type='percent', type_instance='mem_util',
+                            values=[float(gpu.find('utilization/memory_util').text.split()[0])])
                 vl.dispatch(type='power', type_instance='draw',
                             values=[float(gpu.find('power_readings/power_draw').text.split()[0])])
 
+collectd.register_init(restore_sigchld)
 collectd.register_read(read)
 
